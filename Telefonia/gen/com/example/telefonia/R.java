e0956e34d7023cae3e3a6484c9b5882bd8e6ead1
sadan0707 <sadan0707@gmail.com>
2014-12-02 00:23:38 +0100

    wersja z kilkoma dodatkowymi widokoami. Komorka

--- Telefonia/gen/com/example/telefonia/R.java
+++ Telefonia/gen/com/example/telefonia/R.java
@@ -25,27 +25,46 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int TextView02=0x7f070002;
-        public static final int TextView04=0x7f070006;
-        public static final int TextView06=0x7f070008;
-        public static final int TextView08=0x7f07000a;
-        public static final int TextView10=0x7f07000d;
-        public static final int TextView12=0x7f070010;
-        public static final int TextView14=0x7f07000f;
-        public static final int btn_inf_wifi=0x7f070000;
-        public static final int btn_wifi_on_off=0x7f070001;
-        public static final int textView1=0x7f070003;
-        public static final int wifi_bssid=0x7f07000b;
-        public static final int wifi_id_sieci=0x7f07000c;
-        public static final int wifi_ip=0x7f070005;
-        public static final int wifi_mac_adres=0x7f070007;
-        public static final int wifi_moc=0x7f070004;
-        public static final int wifi_speed=0x7f07000e;
-        public static final int wifi_ssid=0x7f070009;
-        public static final int wifi_status=0x7f070011;
+        public static final int TextView01=0x7f070003;
+        public static final int TextView03=0x7f070005;
+        public static final int TextView06=0x7f070017;
+        public static final int TextView08=0x7f070019;
+        public static final int TextView10=0x7f07001c;
+        public static final int TextView12=0x7f07001f;
+        public static final int TextView14=0x7f07001e;
+        public static final int btn_akcelerometr=0x7f07000a;
+        public static final int btn_bateria=0x7f07000c;
+        public static final int btn_inf_wifi=0x7f070012;
+        public static final int btn_komorka=0x7f070009;
+        public static final int btn_komorka_info=0x7f070000;
+        public static final int btn_kompas=0x7f07000b;
+        public static final int btn_pomiary=0x7f070010;
+        public static final int btn_powrot_komorka=0x7f070007;
+        public static final int btn_powrot_pomiary=0x7f07000f;
+        public static final int btn_powrot_wifi=0x7f070021;
+        public static final int btn_poziomica=0x7f07000d;
+        public static final int btn_raport_zbiorczy=0x7f07000e;
+        public static final int btn_rejestr=0x7f070011;
+        public static final int btn_wifi=0x7f070008;
+        public static final int btn_wifi_on_off=0x7f070013;
+        public static final int textView1=0x7f070001;
+        public static final int text_imei=0x7f070002;
+        public static final int text_imei_sv=0x7f070004;
+        public static final int text_nazwa_operatora=0x7f070006;
+        public static final int wifi_bssid=0x7f07001a;
+        public static final int wifi_id_sieci=0x7f07001b;
+        public static final int wifi_ip=0x7f070015;
+        public static final int wifi_mac_adres=0x7f070016;
+        public static final int wifi_moc=0x7f070014;
+        public static final int wifi_speed=0x7f07001d;
+        public static final int wifi_ssid=0x7f070018;
+        public static final int wifi_status=0x7f070020;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_komorla=0x7f030000;
+        public static final int activity_pomiary=0x7f030001;
+        public static final int activity_start=0x7f030002;
+        public static final int activity_wifi=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f060000;
--- Telefonia/src/com/example/telefonia/MainActivity.java
+++ Telefonia/src/com/example/telefonia/MainActivity.java
@@ -11,6 +11,7 @@ import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.telephony.SignalStrength;
+import android.telephony.TelephonyManager;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -30,30 +31,151 @@ public class MainActivity extends Activity { // >>>><< Telefonia - WERSJA 2 >><<
 	WifiManager zarzadca_wifi;
 	WifiInfo inf_wifi;
 	
+	TelephonyManager komorka;
+	
 	int wifi_ip_adres, wifi_sila_sygnalu, wifi_id, wifi_speed;
-	String wifi_mac_adress, wifi_ssid, wifi_bssid;
+	String wifi_mac_adress, wifi_ssid, wifi_bssid, imei;
 	
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_start);
 		
-		Button btn_inf_wifi = (Button)findViewById(R.id.btn_inf_wifi);
+		EkranStart();
 		
-		btn_inf_wifi.setOnClickListener(
-				new OnClickListener() {
-					public void onClick(View v) {
-						InformacjeWifi();
-						
-					}
-				});
-
+		
+		
+		
+		
 	}
+
+		private void EkranStart() {
+		
+			setContentView(R.layout.activity_start);
+			
+			Button btn_pomiary = (Button)findViewById(R.id.btn_pomiary);
+			Button btn_rejestr = (Button)findViewById(R.id.btn_rejestr);
+			
+			btn_pomiary.setOnClickListener(
+					new OnClickListener() {
+						public void onClick(View v) {
+							EkranPomiary();
+						}
+					});
+			
+		// TODO Auto-generated method stub
+		
+	}
+
+		protected void EkranPomiary() {
+		
+			setContentView(R.layout.activity_pomiary);
+			
+			Button btn_wifi = (Button)findViewById(R.id.btn_wifi);
+			Button btn_komorka = (Button)findViewById(R.id.btn_komorka);
+			
+			Button btn_powrot_pomiary = (Button)findViewById(R.id.btn_powrot_pomiary);
+			
+			btn_wifi.setOnClickListener(
+					new OnClickListener() {
+						public void onClick(View v) {
+							EkranWifi();
+							
+						}
+					});
+			
+			btn_komorka.setOnClickListener(
+					new OnClickListener() {
+						public void onClick(View v) {
+							EkranKomorka();
+							
+						}
+					});
+
+
+			btn_powrot_pomiary.setOnClickListener(
+					new OnClickListener() {
+						public void onClick(View v) {
+							EkranStart();
+							
+						}
+					});
+
+		}
+
+		protected void EkranKomorka() {
+			setContentView(R.layout.activity_komorla);
+			
+			Button btn_komorka_info = (Button)findViewById(R.id.btn_komorka_info);
+			Button btn_powrot_komorka = (Button)findViewById(R.id.btn_powrot_komorka);
+			
+			btn_komorka_info.setOnClickListener(
+					new OnClickListener() {
+						public void onClick(View v) {
+							InfoKomorka();
+							
+						}
+					});
+
+			btn_powrot_komorka.setOnClickListener(
+					new OnClickListener() {
+						public void onClick(View v) {
+							EkranPomiary();
+							
+						}
+					});
+			
+		}
+
+		protected void InfoKomorka() {
+			
+			TextView text_imei = (TextView)findViewById(R.id.text_imei);
+			TextView text_imei_sv = (TextView)findViewById(R.id.text_imei_sv);
+			TextView text_nazwa_operatora = (TextView)findViewById(R.id.text_nazwa_operatora);
+			
+			TelephonyManager komorka = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+			
+			String imei = komorka.getDeviceId();
+			String imei_sv = komorka.getDeviceSoftwareVersion();
+			String nazwa_operatora = komorka.getNetworkOperatorName();
+			
+			text_imei.setText(imei);
+			text_imei_sv.setText(imei_sv);
+			text_nazwa_operatora.setText(nazwa_operatora);
+			
+		}
+
+		protected void EkranWifi() {
+			setContentView(R.layout.activity_wifi);
+			
+			Button btn_inf_wifi = (Button)findViewById(R.id.btn_inf_wifi);
+			Button btn_powrot_wifi = (Button)findViewById(R.id.btn_powrot_wifi);
+			ToggleButton btn_wifi_on_off = (ToggleButton)findViewById(R.id.btn_wifi_on_off);
+			
+			btn_inf_wifi.setOnClickListener(
+					new OnClickListener() {
+						public void onClick(View v) {
+							InformacjeWifi();
+							
+						}
+					});
+			
+			btn_powrot_wifi.setOnClickListener(
+					new OnClickListener() {
+						public void onClick(View v) {
+							EkranPomiary();
+							
+						}
+					});
+			
+
+			
+						
+		}
 
 		protected void InformacjeWifi() {
 		
-			
-			ToggleButton btn_wifi_on_off = (ToggleButton)findViewById(R.id.btn_wifi_on_off);
+
 			
 			TextView text_wifi_moc = (TextView)findViewById(R.id.wifi_moc);
 			TextView text_wifi_ip = (TextView)findViewById(R.id.wifi_ip);
@@ -63,37 +185,42 @@ public class MainActivity extends Activity { // >>>><< Telefonia - WERSJA 2 >><<
 			TextView text_wifi_id = (TextView)findViewById(R.id.wifi_id_sieci);
 			TextView text_wifi_speed = (TextView)findViewById(R.id.wifi_speed);
 			TextView text_wifi_status = (TextView)findViewById(R.id.wifi_status);
+		
+			polaczenie = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
 			
-		polaczenie = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+			zarzadca_wifi = (WifiManager)getSystemService(Context.WIFI_SERVICE);
+			
+			inf_wifi = zarzadca_wifi.getConnectionInfo();
+			
+			wifi_sila_sygnalu = inf_wifi.getRssi();
+			wifi_ip_adres = inf_wifi.getIpAddress();
+			wifi_mac_adress = inf_wifi.getMacAddress();
+			wifi_ssid = inf_wifi.getSSID();
+			wifi_bssid = inf_wifi.getBSSID();
+			wifi_id = inf_wifi.getNetworkId();
+			wifi_speed = inf_wifi.getLinkSpeed();
+			
+			
+			
+			wifi = polaczenie.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+			cdma = polaczenie.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+			
+			
+			text_wifi_moc.setText(String.valueOf(wifi_sila_sygnalu)+ " dBm");
+			text_wifi_ip.setText(String.valueOf(wifi_ip_adres));
+			text_wifi_mac.setText(wifi_mac_adress);
+			text_wifi_ssid.setText(wifi_ssid);
+			text_wifi_bssid.setText(wifi_bssid);
+			text_wifi_id.setText(String.valueOf(wifi_id));
+			text_wifi_speed.setText(String.valueOf(wifi_speed)+" Mb/s");
 		
-		zarzadca_wifi = (WifiManager)getSystemService(Context.WIFI_SERVICE);
+			if(wifi.isConnected())
+				text_wifi_status.setText("POгеCZONO");
+			else
+				text_wifi_status.setText("NIE POгеCZONO");
+
+			
 		
-		inf_wifi = zarzadca_wifi.getConnectionInfo();
-		
-		wifi_sila_sygnalu = inf_wifi.getRssi();
-		wifi_ip_adres = inf_wifi.getIpAddress();
-		wifi_mac_adress = inf_wifi.getMacAddress();
-		wifi_ssid = inf_wifi.getSSID();
-		wifi_bssid = inf_wifi.getBSSID();
-		wifi_id = inf_wifi.getNetworkId();
-		wifi_speed = inf_wifi.getLinkSpeed();
-		
-		text_wifi_moc.setText(String.valueOf(wifi_sila_sygnalu)+ " dBm");
-		text_wifi_ip.setText(String.valueOf(wifi_ip_adres));
-		text_wifi_mac.setText(wifi_mac_adress);
-		text_wifi_ssid.setText(wifi_ssid);
-		text_wifi_bssid.setText(wifi_bssid);
-		text_wifi_id.setText(String.valueOf(wifi_id));
-		text_wifi_speed.setText(String.valueOf(wifi_speed)+" Mb/s");
-		
-		
-		wifi = polaczenie.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-		cdma = polaczenie.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
-		
-		if(wifi.isConnected())
-			text_wifi_status.setText("POгеCZONO");
-		else
-			text_wifi_status.setText("NIE POгеCZONO");
 		/*{
 		Toast.makeText(this, "Wifi POгеCZONA!!!!", Toast.LENGTH_LONG).show();
 		Toast.makeText(this, "Ip Adres to" + wifi_ip_adres , Toast.LENGTH_LONG).show();
